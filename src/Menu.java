04932487b039ba15e2879d52328eb89b5735ef20
sergiomp36 <sergiomp36@educastur.es>
2025-01-08 21:19:33 +0100

    actualizaciónEnero25

--- src/Menu.java
+++ src/Menu.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class Menu {
 	Scanner input = new Scanner(System.in);
 	
-	/*
-	 * menú para escoger el número de jugadores 
-	 * 
-	 * solo acepta entre 3 y 8, y es el parámetro que se le pasa
-	 * como argumento al constructor de la clase Partida
-	*/
+
 	public int numeroJugadores() {
 		int opcion;
 		System.out.print("Introduzca el número de jugadores: ");
@@ -20,15 +15,7 @@ public class Menu {
 		return opcion;
 	}
 	
-	/*
-	 * Este es el menú que se muestra nada más empezar.
-	 * Para mostrar las reglas y la información, que solo es mostrar texto,
-	 * y es bastante, creé la clase Utilities.
-	 * Esa clase es para meter los metodos más coñazo y que no emborronen el resto
-	 * de clases.
-	 * Las reglas y la información están sin escribir, pero para cuando se escriban se hará
-	 * en la clase Utilities (por esto está el segundo if)
-	*/
+
 	public int menuInicial() {
 		int opcion;
 		System.out.println("------ COLD WAR ------");
--- src/Partida.java
+++ src/Partida.java
@@ -6,43 +6,25 @@ public class Partida {
 	
 	//variables
 	
-	/*
-	 * El ArrayList es un Array de objetos de tipo Equipo. Al ser una estructura dinámica
-	 * se pueden ir añadiendo equipos según se van creando con participantes.add (linea 43).
-	 * 
-	 * 
-	*/
+
 	ArrayList <Equipo> participantes = new ArrayList<Equipo>();
 	public int numJugadores;
 	
 	//CONSTRUCTOR
 	
-	/*
-	 * El número de jugadores se le pasa al constructor. Si vas a la clase main,
-	 * verás que el número de jugadores lo determina un menú de la clase menú para
-	 * que introduzca el número el usuario.
-	*/
+	
 	public Partida(int numJugadores) {
 		this.numJugadores=numJugadores;
 	}
 	
 	//metodos
-	
-	/*
-	 * Este es el método principal que se ejecuta desde el método main
-	 * y sirve principalmente para llamar a otros métodos de esta clase.
-	*/
 	public void jugar() {
 		escogerNombreEquipo();
 		escogerPais();
 		repartirVidas();
 		
-		
 	}
-	/* 
-	 * se le da un nombre al equipo (escogido por el usuario) que puede ser cualquier String.
-	 * (en el print de la linea 31 pone (i+1) para que ponga bien el número y se salte el 0).
-	*/
+	
 	public void escogerNombreEquipo() {
 		for (int i = 0; i<numJugadores;i++) {
 			System.out.print("Jugador "+(i+1)+", escoge un nombre: ");
@@ -52,26 +34,20 @@ public class Partida {
 	}
 	
 	
-	/* 
-	 * asigna un país en función de un número q introduce el usuario.
-	 * ese número se le pasa al setter que está en la clase Equipo 
-	 * y este coge ese elemento del array de países que está en esa clase también.
-	*/
+	
 	public void escogerPais() {
 		for (int i=0;i<numJugadores;i++) {
-			System.out.print(participantes.get(i).getNombre()+", escoge un país \n(1) Alemania, (2)Francia, (3)Italia, (4)Eslovaquia\n(5)República Checa, (6)Polonia, (7)Hungría, (8)Austria: ");
+			System.out.println(participantes.get(i).getNombre()+", escoge un país \n(1) Alemania, (2)Francia, (3)Italia, (4)Eslovaquia\n(5)República Checa, (6)Polonia, (7)Hungría, (8)Austria: ");
 			int pais = input.nextInt();
 			while (pais<1 || pais>8) {
 				System.out.print("ERROR: Introduzca un país correcto: ");
 				pais = input.nextInt();
 			}
 			participantes.get(i).setPais(pais);
-			System.out.println("Has escogido "+participantes.get(i).getPais());
 		}
 	}
 	
 	
-	//reparte vidas en funcion del país: todos 200 menos Francia que tiene un 20% más
 	public void repartirVidas() {
 		for (int i=0;i<numJugadores;i++) {
 			switch (participantes.get(i).getPais()) {
