06884600b80cb7705657c8003de1767da8ed1066
sergiomp36 <sergiomp36@educastur.es>
2025-01-23 21:25:39 +0100

    RESET
    DEP

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Menu menu=new Menu();
 		
-		int opcionInicial=menu.menuInicial();
-		if (opcionInicial==0) {
-			System.exit(0);;
-		}
-		
-
-		Partida partida1 = new Partida(menu.numeroJugadores());
-		partida1.jugar();
 	}
 
 }
