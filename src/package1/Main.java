e8a00df0a644ef9b51ac332a02ae4a9cdf80fe69
RFM16975 <rfm16975@educastur.es>
2025-05-09 16:38:51 +0200

    base de datos
    Creado enlace con la base de datos

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -1,27 +1,28 @@
 package package1;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Menu menu = new Menu();
-		AudioPlayer.ReproducirAudio();
-		int x;
-		
-		do {
-		x = menu.menuInicial();
-		if (x==0)
-			System.exit(0);
-		
-		else if (x==1){
-			Partida partida = new Partida();
-			partida.jugar(menu.menuNumJugadores());
-			
-		}
-		
-		}while(x!=0);
-		
-		
-}
 
-	
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        AudioPlayer.ReproducirAudio();
+        int x;
+
+        do {
+            x = menu.menuInicial();
+            if (x == 0)
+                System.exit(0);
+
+            else if (x == 1) {
+            	AudioPlayer.ReproducirAudio2();
+                Partida partida = new Partida();
+                partida.jugar(menu.menuNumJugadores());
+            } else if (x == 4) {
+            	AudioPlayer.ReproducirAudio2();
+                Partida partida = new Partida();
+                partida.cargarUltimaPartida();
+                partida.jugar(partida.jugadoresVivos());
+            }
+
+        } while (x != 0);
+    }
 }
